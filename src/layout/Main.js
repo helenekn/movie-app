2d2843e4805e175dc2237b8cfe1ded2b6539fd36
Helen <redkina2508@gmail.com>
2023-10-12 22:52:28 +0300

    add env

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,43 +1,46 @@
-import React from 'react';
-import { MovieList } from '../components/MovieList';
-import { Preloader } from '../components/Preloader';
-import { InputSearch } from '../components/InputSearch';
+import React from "react";
+import { MovieList } from "../components/MovieList";
+import { Preloader } from "../components/Preloader";
+import { InputSearch } from "../components/InputSearch";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class Main extends React.Component {
-    state = {
-        movieList: [],
-    }
+   state = {
+      movieList: [],
+   };
 
-    componentDidMount() {
-        fetch('http://www.omdbapi.com/?apikey=545d2dce&s=titanic')
-            .then(response => response.json())
-            .then(data => this.setState({movieList: data.Search}))
-    }
+   componentDidMount() {
+      fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=titanic`)
+         .then((response) => response.json())
+         .then((data) => this.setState({ movieList: data.Search }));
+   }
 
-    searchMovies = (str, type = 'all') => {
-        fetch(`http://www.omdbapi.com/?apikey=545d2dce&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+   searchMovies = (str, type = "all") => {
+      fetch(
+         `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+            type !== "all" ? `&type=${type}` : ""
+         }`
+      )
+         // http://www.omdbapi.com/?apikey=545d2dce&s=&titanic&type=series
 
+         .then((response) => response.json())
+         .then((data) => this.setState({ movieList: data.Search }));
+   };
 
-        // http://www.omdbapi.com/?apikey=545d2dce&s=&titanic&type=series
-
-        
-            .then(response => response.json())
-            .then(data => this.setState({movieList: data.Search}))
-    }
- 
-    render() {
-        const { movieList } = this.state;
-        return (
-            <main className='main container'>
-                <InputSearch searchMovies={this.searchMovies}/>
-                {
-                    movieList.length ?
-                        (<MovieList movieList={this.state.movieList} />)
-                        : <Preloader />
-                }
-            </main>
-        )
-    }
+   render() {
+      const { movieList } = this.state;
+      return (
+         <main className="main container">
+            <InputSearch searchMovies={this.searchMovies} />
+            {movieList.length ? (
+               <MovieList movieList={this.state.movieList} />
+            ) : (
+               <Preloader />
+            )}
+         </main>
+      );
+   }
 }
 
 export { Main };
